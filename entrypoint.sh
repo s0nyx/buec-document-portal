f2ee839b7e1612a3bccd42619c96880eb83adaea
s0nyx <slupoiu69@gmail.com>
2026-07-14 16:01:54 +0100

    addin superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,15 @@ set -eu
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+if [ -n "${DJANGO_SUPERUSER_USERNAME:-}" ] && \
+   [ -n "${DJANGO_SUPERUSER_EMAIL:-}" ] && \
+   [ -n "${DJANGO_SUPERUSER_PASSWORD:-}" ]; then
+    python manage.py createsuperuser --noinput || true
+fi
+
 exec gunicorn config.wsgi:application \
   --bind "0.0.0.0:${PORT:-8000}" \
-  --workers "${GUNICORN_WORKERS:-3}" \
+  --workers "${GUNICORN_WORKERS:-1}" \
   --threads "${GUNICORN_THREADS:-2}" \
   --timeout "${GUNICORN_TIMEOUT:-60}" \
   --error-logfile -
