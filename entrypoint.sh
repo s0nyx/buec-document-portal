8b4f3e85fbc454f1aa585ca7ad7980e456ac1347
s0nyx <slupoiu69@gmail.com>
2026-07-14 15:25:32 +0100

    Configure production port

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 exec gunicorn config.wsgi:application \
-  --bind 0.0.0.0:8000 \
+  --bind "0.0.0.0:${PORT:-8000}" \
   --workers "${GUNICORN_WORKERS:-3}" \
   --threads "${GUNICORN_THREADS:-2}" \
   --timeout "${GUNICORN_TIMEOUT:-60}" \
